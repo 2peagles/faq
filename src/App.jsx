8ed1135e797e895ce39565e8611e38cd94d2a586
Precious <precioud.d.bey@gmail.com>
2022-12-17 00:27:13 -0500

    arrows fixed

--- src/App.jsx
+++ src/App.jsx
@@ -10,28 +10,53 @@ function App() {
         <div className='faq_section'>
           <h1>FAQ</h1>
             <div className='dropdown '>
-                <h4>How many team members can I invite? <span><img src="/images/icon-arrow-down.svg"/></span></h4>
-                  <p>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan. </p>
+              <div className='row_arrow'>
+                 <h4>How many team members can I invite? </h4>
+                <span><img src="/images/icon-arrow-down.svg" /></span>
+              </div>
+              <div className='dropdown-content'>
+                  <p className=''>You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan. </p>
+              </div>
             </div>
 
-            <div className='dropdown'>
-               <h4> What is the maximum file upload size? <span><img src="/images/icon-arrow-down.svg" /></span></h4>
+            <div className='dropdown '>
+              <div className="div row_arrow">
+               <h4> What is the maximum file upload size?</h4>
+               <span><img src="/images/icon-arrow-down.svg" /></span>
+          </div>
+            <div className=" dropdown-content">
                <p>No more than 2GB. All files in your account must fit your allotted storage space. </p>
+          </div>
             </div>
 
             <div className='dropdown '>
-              <h4> How do I reset my password?<span><img src="/images/icon-arrow-down.svg" /></span></h4>
-              <p>Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you. </p>
+              <div className=" row_arrow">
+                <h4> How do I reset my password?</h4>
+                <span><img src="/images/icon-arrow-down.svg" /></span>
             </div>
-
-            <div className='dropdown '>
-              <h4>Can I cancel my subscription? <span><img src="/images/icon-arrow-down.svg" /></span></h4>
-              <p>Yes! Send us a message and we’ll process your request no questions asked. </p>
+             <div className=" dropdown-content">
+                 <p>Click “Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you. </p>
+               </div>
             </div>
 
             <div className='dropdown'>
-              <h4>Do you provide additional support?  <span><img src="/images/icon-arrow-down.svg" /></span></h4>
-              <p>Chat and email support is available 24/7. Phone lines are open during normal business hours. </p>
+              <div className="row_arrow">
+                 <h4>Can I cancel my subscription?</h4>
+                  <span><img src="/images/icon-arrow-down.svg" /></span>
+              </div>
+              <div className=" dropdown-content">
+              <p >Yes! Send us a message and we’ll process your request no questions asked. </p>
+              </div>
+            </div>
+
+            <div className='dropdown '>
+              <div className="row_arrow">
+                <h4>Do you provide additional support? </h4>
+                  <span><img src="/images/icon-arrow-down.svg" /></span>
+               </div>
+                <div className="dropdown-content">
+                  <p>Chat and email support is available 24/7. Phone lines are open during normal business hours. </p>
+              </div>
           </div>
         </div>
     </section>
